98b35d70ea61512c4c03d3b9ae6ae55e6f5bdaa6
yaoyao <yao_zhou@icloud.com>
2017-05-02 18:14:12 -0700

    step7 upload resume

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'simple_form'
 
 gem 'font-awesome-rails'
 
+gem 'carrierwave'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
 
   namespace :admin do
     resources :jobs do
@@ -13,6 +15,8 @@ Rails.application.routes.draw do
       end
     end
   end
+
+
 
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
